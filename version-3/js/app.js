a9cc0a73583bb6909dce0c91fd4a4961d7367280
Conor Hooley <cchooley@gmail.com>
2018-03-22 20:21:57 -0600

    Adding drill 3 prior to first commit

--- version-3/js/app.js
+++ version-3/js/app.js
@@ -2,17 +2,19 @@
 var drawGrid = document.getElementById('paintBoard');
 var drawPalette = document.getElementById('paletteBoard');
 
+createCanvas();
+createPalette();
+
 function createCanvas() {
   for (var i = 0; i < 560; i++) {
     var newDiv = document.createElement('div');
     newDiv.classList.add('paintSquare');
     drawGrid.appendChild(newDiv);
     newDiv.addEventListener('click', function setColor(target) {
-      event.target.style.backgroundColor = document.querySelector('.colorSelect').style.backgroundColor
+      event.target.style.backgroundColor = document.querySelector('.colorSelect').style.backgroundColor;
     })
   }
 }
-createCanvas();
 
 function createPalette() {
   for (var i = 0; i < 10; i++) {
@@ -20,21 +22,21 @@ function createPalette() {
     newDiv.id = i;
     newDiv.classList.add('paletteColor');
     if (newDiv.id === "0") {
-      newDiv.style.backgroundColor = 'red'
+      newDiv.style.backgroundColor = 'red';
     } else if (newDiv.id ===  "1") {
-      newDiv.style.backgroundColor = 'blue'
+      newDiv.style.backgroundColor = 'blue';
     } else if (newDiv.id === "2") {
-      newDiv.style.backgroundColor = 'yellow'
+      newDiv.style.backgroundColor = 'yellow';
     } else if (newDiv.id === "3") {
-      newDiv.style.backgroundColor = 'grey'
+      newDiv.style.backgroundColor = 'grey';
     } else if (newDiv.id === "4") {
-      newDiv.style.backgroundColor = 'brown'
+      newDiv.style.backgroundColor = 'brown';
     } else if (newDiv.id === "5") {
-      newDiv.style.backgroundColor = 'black'
+      newDiv.style.backgroundColor = 'black';
     } else if (newDiv.id === "6") {
-      newDiv.style.backgroundColor = 'purple'
+      newDiv.style.backgroundColor = 'purple';
     } else if (newDiv.id === "7") {
-      newDiv.style.backgroundColor = 'green'
+      newDiv.style.backgroundColor = 'green';
     } else if (newDiv.id === "8"){
       newDiv.style.backgroundColor = 'white';
     } else {
@@ -44,9 +46,7 @@ function createPalette() {
     drawPalette.appendChild(newDiv);
 
     newDiv.addEventListener('click', function getColor(target) {
-      document.querySelector('.colorSelect').style.backgroundColor = event.target.style.backgroundColor
+      document.querySelector('.colorSelect').style.backgroundColor = event.target.style.backgroundColor;
     })
-
   }
 }
-createPalette();
